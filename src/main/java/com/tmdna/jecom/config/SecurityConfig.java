5338a51fe973ec762149d4dd26a68520dce92f77
Olexandr-Savelev <a.savelev.krm@gmail.com>
2025-08-05 00:02:43 +0300

    add rsa key properties and jwt encoder/decoder beans

--- src/main/java/com/tmdna/jecom/config/SecurityConfig.java
+++ src/main/java/com/tmdna/jecom/config/SecurityConfig.java
@@ -1,31 +1,70 @@
 package com.tmdna.jecom.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final RsaKeyProperties rsaKeyProperties;
+
+    public SecurityConfig(RsaKeyProperties rsaKeyProperties) {
+        this.rsaKeyProperties = rsaKeyProperties;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        return new InMemoryUserDetailsManager(User.withUsername("tmdna")
+                .password("123")
+                .authorities("read")
+                .build());
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        mather ->
-                                mather
-                                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
-                                        .requestMatchers("/api/images/upload").permitAll()
-                                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                .authorizeHttpRequests(matcher ->
+                        matcher
+                                .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+                                .requestMatchers("/api/images/upload").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                 )
                 .authorizeHttpRequests(matcher -> matcher.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeyProperties.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeyProperties.publicKey()).privateKey(rsaKeyProperties.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
